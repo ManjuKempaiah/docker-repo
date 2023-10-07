e82012efbc6b7db15727ee62f6cdf9d868d981f2
ManjuKempaiah <136099797+ManjuKempaiah@users.noreply.github.com>
2023-10-07 12:53:54 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from redis import Redis
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return '<h1 style="color:red">Welcome to Java Home  </h1>'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True,port=80)
