b7bd83b9df3fce72a2a0acdeaf199dc8b805dc67
ManjuKempaiah <136099797+ManjuKempaiah@users.noreply.github.com>
2023-11-17 10:33:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return '<h1 style="color:green">Welcome to Java Home Hari  </h1>'
+    return '<h1 style="color:blue">Welcome to Java Home Manju  </h1>'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True,port=80)
